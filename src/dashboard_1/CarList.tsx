892909b5bfb52a10bc6ee52735a821ec1bc36c2d
gokulgs078 <gokul2023gs@gmail.com>
2025-01-17 12:16:09 +0530

    feat:responsiveness_refined

--- src/dashboard_1/CarList.tsx
+++ src/dashboard_1/CarList.tsx
@@ -58,7 +58,7 @@ const CarList: React.FC = () => {
         </a>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-6">
         {filteredCars.map((car) => (
           <CarCard
             key={car.id}
